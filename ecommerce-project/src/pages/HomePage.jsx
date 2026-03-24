4f8325b99347385a12120ede8ca6069c07e8e5e3
dinhkhanh <trandinhkhanh0318@gmail.com>
2026-03-24 21:34:02 +0700

    Fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -4,6 +4,13 @@ import CheckmarkIcon from "../assets/images/icons/checkmark.png";
 import { products } from "../data/products.js";
 
 const HomePage = () => {
+  fetch("http://localhost:3000/api/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
   return (
     <>
       <title>Ecommerce Project</title>
